6ab89579ed415cb9f695f2a0bceea0a2aac4264e
Andrew H <andrewdev@Andrews-MacBook-Air.local>
2022-05-18 18:01:15 -0400

    added alert device controller

--- SmartHome/ViewControllers/DeviceTableViewController.swift
+++ SmartHome/ViewControllers/DeviceTableViewController.swift
@@ -16,20 +16,37 @@ class DeviceTableViewController: UITableViewController {
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return DeviceController.sharedinstance.devices.count
     }
-
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "deviceCell", for: indexPath) as? DeviceTableViewCell else {return UITableViewCell()}
+        let device = DeviceController.sharedinstance.devices[indexPath.row]
+    //TODO: create updateviews
+        //cell.updateViews()
 
         return cell
     }
-    */
+    
+    private func presentNewDeviceAlertController() {
+        let alertController = UIAlertController(title: "New Device", message: "Enter name of device below", preferredStyle: .alert)
+        alertController.addTextField { textField in
+            textField.placeholder = "New Device Name"
+        }
+        let dismissAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(dismissAction)
+        let confirmAction = UIAlertAction(title: "Create", style: .default) { _ in
+            guard let contentTextField = alertController.textFields?.first,
+                  let name = contentTextField.text else {return}
+            DeviceController.sharedinstance.createDevice(name: name)
+            self.tableView.reloadData()
+        }
+        alertController.addAction(confirmAction)
+        present(alertController, animated: true)
+    }
 
+    // Actions
     @IBAction func addButtonTapped(_ sender: Any) {
+        presentNewDeviceAlertController()
     }
     
     
